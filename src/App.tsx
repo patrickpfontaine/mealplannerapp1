1609310db3663ce5787d2ee46a32a21530b0b15b
patrickpfontaine <patrick.herve.fontaine@gmail.com>
2024-11-05 15:21:19 -0600

    added hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 //import './App.css';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HeroSection from './components/Hero/HeroSection';
@@ -8,6 +8,25 @@ function App() {
     <div className="App">
       <HeroSection /> 
     </div>
+  );
+}
+
+export default App;
+*/
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import HeroSection from './components/Hero/HeroSection';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Define a route for HeroSection */}
+          <Route path="/" element={<HeroSection />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
